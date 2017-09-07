4925fc677259c5c4536e919edc43ce7072b5ea0a
Sam Lau <samlau95@gmail.com>
2017-09-07 12:14:48 -0700

    hw/hw2 release

--- hw/hw2/ok_tests/q01.py
+++ hw/hw2/ok_tests/q01.py
@@ -8,15 +8,9 @@ test = {
           'code': r"""
           >>> isinstance(bus, pd.DataFrame)
           True
-          >>> bus.shape == (6315, 9)
-          True
           >>> isinstance(ins, pd.DataFrame)
           True
-          >>> ins.shape == (15430, 4)
-          True
           >>> isinstance(vio, pd.DataFrame)
-          True
-          >>> vio.shape == (40936, 3)
           True
           """,
           'hidden': False,
