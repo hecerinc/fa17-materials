67caf145b9466a1170e5d0dcd30d6345fd9ea7cb
Nhi Quach <nhiquach@berkeley.edu>
2017-10-17 13:31:55 -0700

    labs/lab07 release

---
+++ labs/lab07/ok_tests/q05.py
@@ -0,0 +1,25 @@
+
+test = {
+  'name': 'q05',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+'code': r"""
+>>> (np.allclose(left_endpoint, np.array([ 2.44691185,  2.17881283,  2.06389856,  2.03693334,  1.98397152,
+True
+>>>         1.97189622])) )
+
+""",
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
