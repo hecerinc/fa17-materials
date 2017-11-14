7d54d4dc8280a23f4ec962613b2b2f11516b6eeb
Nhi Quach <nhiquach@berkeley.edu>
2017-11-14 10:32:48 -0800

    labs/lab11 release

---
+++ labs/lab11/ok_tests/q05.py
@@ -0,0 +1,26 @@
+
+test = {
+  'name': 'q05',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+'code': r"""
+>>> best_feature_set == ['RM', 'LSTAT', 'PTRATIO']
+True
+>>> np.abs(best_error - 30.748202560092775) < 1e-3
+True
+
+""",
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
