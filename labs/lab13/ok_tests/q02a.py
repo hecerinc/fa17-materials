81b5561b6185714571318236cc14e6a7ca4ffd3a
Jake Soloff <jake_soloff@berkeley.edu>
2017-12-05 01:27:12 -0800

    add lab13 to materials

---
+++ labs/lab13/ok_tests/q02a.py
@@ -0,0 +1,24 @@
+
+test = {
+  'name': 'q02a',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+'code': r"""
+>>> tree2.xpath(expr)[0].get("href") == '/wiki/Lists_of_unsolved_problems'
+True
+
+""",
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
